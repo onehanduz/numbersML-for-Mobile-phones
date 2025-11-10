7cd331df5d5e2a341815c675c3b5efabc302c005
onehanduz <onehanduz@gmail.com>
2025-11-10 17:14:29 +0500

    cards for me

--- numbers/app.js
+++ numbers/app.js
@@ -1,122 +1,95 @@
-let counterClick = 0;
-let resultTime = [];
-let memoNums = ["START"];
-
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
+function getRandomInt(objd) {
+  const minCeiled = Math.ceil(objd.minValue);
+  const maxFloored = Math.floor(objd.maxValue);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-function memoReturn() {
-  resultTime[resultTime.length - 1].stop = Date.now();
-  counterClick = 0;
-  document.getElementById("memoMidNum").innerHTML = memoNums[counterClick];
-  document.getElementById("statusBar").innerHTML = `0/${memoNums.length - 1}`;
-}
-function memoPrev() {
-  if (counterClick > 0) {
-    resultTime[resultTime.length - 1].stop = Date.now();
-    counterClick--;
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
-  document.getElementById("memoMidNum").innerHTML = memoNums[counterClick];
-  document.getElementById("statusBar").innerHTML = `${counterClick}/${
-    memoNums.length - 1
-  }`;
-  if (counterClick != 0) {
-    resultTime.push({ counter: counterClick, start: Date.now(), stop: 0 });
-  }
-}
-function memoNext() {
-  if (resultTime[resultTime.length - 1] && counterClick > 0) {
-    resultTime[resultTime.length - 1].stop = Date.now();
-  }
-  if (memoNums.length - 1 == counterClick) {
-    document.getElementById("memoEnd").click();
-  }
-  if (counterClick < memoNums.length - 1) {
-    counterClick++;
-  }
-  document.getElementById("memoMidNum").innerHTML = memoNums[counterClick];
-  document.getElementById("statusBar").innerHTML = `${counterClick}/${
-    memoNums.length - 1
-  }`;
-  resultTime.push({ counter: counterClick, start: Date.now(), stop: 0 });
 }
 
-function easyGenNum(objd) {
-  let digit = twoDigits;
-  if (objd.numberSystem == 3) digit = threeDigits;
-  let element1 = getRandomInt(objd.minValue, objd.maxValue);
-  let element2 = getRandomInt(objd.minValue, objd.maxValue);
-  let element3 = getRandomInt(objd.minValue, objd.maxValue);
-  if (
-    element1 < digit.length &&
-    element2 < digit.length &&
-    element3 < digit.length
-  ) {
+const createNumberRange = (start, end, step = 1) => {
+  const length = Math.floor((end - start) / step) + 1;
+  return Array.from({ length }, (_, index) => start + index * step);
+};
+
+function getRandomElements(objd) {
+  const minCeiled = Math.ceil(objd.minValue);
+  const maxFloored = Math.floor(objd.maxValue) - 1;
+  let elements = createNumberRange(minCeiled, maxFloored);
+  shuffle(elements);
+
+  return elements;
+}
+
+function generateNumber(objd) {
+  let result = "error";
+  let digits = objd.numberSystem == 3 ? threeDigits : twoDigits;
+  if (objd.minValue < digits.length && objd.maxValue <= digits.length) {
+    let element = getRandomInt(objd);
+    result = digits[element];
+  }
+  return result;
+}
+
+function numbersRepeatable(objd) {
+  for (let i = 0; i < objd.amount; i++) {
     if (objd.image == 1) {
-      memoNums.push(digit[element1]);
+      memoNums.push(generateNumber(objd));
     } else if (objd.image == 2) {
-      memoNums.push(digit[element1] + "" + digit[element2]);
+      memoNums.push(generateNumber(objd) + " " + generateNumber(objd));
     } else if (objd.image == 3) {
       memoNums.push(
-        digit[element1] + "" + digit[element2] + "" + digit[element3]
+        generateNumber(objd) +
+          " " +
+          generateNumber(objd) +
+          " " +
+          generateNumber(objd)
       );
     }
   }
 }
 
-function generateNumsRep(objd) {
-  for (let i = 0; i < objd.amount; i++) {
-    easyGenNum(objd);
-  }
-}
-
-function generateNumsUnrepImg(objd) {
-  let newDigits1 = twoDigits.slice(objd.minValue, objd.maxValue);
-  let newDigits2 = twoDigits.slice(objd.minValue, objd.maxValue);
-  let newDigits3 = twoDigits.slice(objd.minValue, objd.maxValue);
-  if (objd.numberSystem == 3) {
-    newDigits1 = threeDigits.slice(objd.minValue, objd.maxValue);
-    newDigits2 = threeDigits.slice(objd.minValue, objd.maxValue);
-    newDigits3 = threeDigits.slice(objd.minValue, objd.maxValue);
-  }
-  let amount = newDigits1.length;
-  for (let i = 0; i < amount; i++) {
-    let element1 = getRandomInt(0, newDigits1.length);
-    let element2 = getRandomInt(0, newDigits2.length);
-    let element3 = getRandomInt(0, newDigits3.length);
+function numbersUnrepeatable(objd) {
+  let digits = objd.numberSystem == 3 ? threeDigits : twoDigits;
+  if (objd.minValue < digits.length && objd.maxValue <= digits.length) {
     if (objd.image == 1) {
-      if (objd.unrepeatable1 == "true") {
-        memoNums.push(newDigits1[element1]);
-        newDigits1.splice(element1, 1);
-      } else {
-        return generateNumsRep(objd);
+      let elements = getRandomElements(objd);
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        memoNums.push(digits[element]);
       }
     } else if (objd.image == 2) {
-      if (
-        objd.unrepeatable3 == "true" &&
-        objd.unrepeatable2 == "false" &&
-        objd.unrepeatable1 == "false"
-      ) {
-        return generateNums(objd);
-      } else {
-        memoNums.push(newDigits1[element1] + "" + newDigits2[element2]);
-        if (objd.unrepeatable1 == "true") newDigits1.splice(element1, 1);
-        if (objd.unrepeatable2 == "true") newDigits2.splice(element2, 1);
+      let elements = getRandomElements(objd);
+      let elements2 = getRandomElements(objd);
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const element2 = elements2[i];
+        memoNums.push(digits[element] + " " + digits(element2));
       }
     } else if (objd.image == 3) {
-      memoNums.push(
-        newDigits1[element1] +
-          "" +
-          newDigits2[element2] +
-          "" +
-          newDigits3[element3]
-      );
-      if (objd.unrepeatable1 == "true") newDigits1.splice(element1, 1);
-      if (objd.unrepeatable2 == "true") newDigits2.splice(element2, 1);
-      if (objd.unrepeatable3 == "true") newDigits3.splice(element3, 1);
+      let elements = getRandomElements(objd);
+      let elements2 = getRandomElements(objd);
+      let elements3 = getRandomElements(objd);
+
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const element2 = elements2[i];
+        const element3 = elements3[i];
+        memoNums.push(
+          digits[element] + " " + digits(element2) + " " + digits(element3)
+        );
+      }
     }
   }
 }
@@ -125,19 +98,20 @@ function generateNums(objd) {
   if (objd.slownumbers) {
     let slownumbers = [...objd.slownumbers];
     for (let i = 0; i < objd.slownumbers.length; i++) {
-      let element = getRandomInt(0, slownumbers.length);
+      let element = getRandomInt({ minValue: 0, maxValue: slownumbers.length });
       memoNums.push(slownumbers[element]);
       slownumbers.splice(element, 1);
     }
   } else {
-    if (
-      objd.unrepeatable1 != "true" &&
-      objd.unrepeatable2 != "true" &&
-      objd.unrepeatable3 != "true"
-    ) {
-      generateNumsRep(objd);
+    if (objd.repeatable == "true") {
+      numbersRepeatable(objd);
     } else {
-      generateNumsUnrepImg(objd);
+      numbersUnrepeatable(objd);
+    }
+    if (objd.sorted == "1") {
+      memoNums.sort((a, b) => parseInt(a) - parseInt(b));
+    } else if (objd.sorted == "2") {
+      memoNums.sort((a, b) => parseInt(b) - parseInt(a));
     }
   }
 }
@@ -150,9 +124,8 @@ function getData(form) {
     minValue: 0,
     maxValue: 100,
     slownumbers: false,
-    unrepeatable1: false,
-    unrepeatable2: false,
-    unrepeatable3: false,
+    repeatable: false,
+    sorted: 0,
     autoAdvanced: false,
   };
   var formData = new FormData(form);
@@ -173,14 +146,11 @@ function getData(form) {
       if (obj.numberSystem == 3) obj.maxValue = 1000;
       if (pair[1] > 0) obj.maxValue = pair[1];
     }
-    if (pair[0] == "unrepeatable1") {
-      obj.unrepeatable1 = pair[1];
+    if (pair[0] == "repeatable") {
+      obj.repeatable = pair[1];
     }
-    if (pair[0] == "unrepeatable2") {
-      obj.unrepeatable2 = pair[1];
-    }
-    if (pair[0] == "unrepeatable3") {
-      obj.unrepeatable3 = pair[1];
+    if (pair[0] == "sorted") {
+      obj.sorted = pair[1];
     }
     if (pair[0] == "slownumbers") {
       if (pair[1] != "") {
@@ -189,7 +159,7 @@ function getData(form) {
       }
     }
     if (pair[0] == "autoAdvanced") {
-      if (pair[1] !== "") obj.autoAdvanced = pair[1];
+      if (pair[1] !== "") obj.autoAdvanced = parseInt(pair[1]) * 10;
     }
   }
   return obj;
--- numbers/result.js
+++ numbers/result.js
@@ -1,5 +1,9 @@
 let startTime;
 let endTime;
+let stop_time = 0;
+let stop_time_counter = 0;
+let pure_time = 0;
+let avaragetime = 0;
 let elapsedTime = 0;
 let timerInterval;
 let interval;
@@ -46,38 +50,41 @@ function pause() {
 }
 
 function calculateResults(resultTime) {
-  let array1 = [];
-  for (let i = 0; i < resultTime.length; i++) {
-    let counter = resultTime[i].counter;
-    let start = resultTime[i].start;
-    let stop = resultTime[i].stop;
-    let res = Number(stop - start);
-    let another = resultTime.reduce((r, n, iw) => {
-      n.counter == counter && r.push(iw);
-      return r;
-    }, []);
-    if (another.length > 1 && another[0] == i) {
-      for (let i = 1; i < another.length; i++) {
-        res = Number(
-          res + resultTime[another[i]].stop - resultTime[another[i]].start
-        );
-      }
-      array1.push({ counter: counter, time: res });
-    } else if (another.length == 1) {
-      array1.push({ counter: counter, time: res });
+  const results = {};
+
+  for (const { counter, start, stop } of resultTime) {
+    const duration = stop - start;
+    results[counter] = (results[counter] || 0) + duration;
+  }
+
+  // Separate counter 0 and others
+  const array1 = [];
+  for (const [counter, total] of Object.entries(results)) {
+    if (Number(counter) === 0) {
+      stop_time += total;
+      stop_time_counter++;
+    } else {
+      array1.push({ counter: Number(counter), time: total });
     }
   }
+
   return array1;
 }
 
 function printHead(resultTime) {
   let times = Number(endTime - startTime);
-
+  avaragetime = Number(
+    (times - stop_time) / resultTime.length - stop_time_counter
+  );
+  pure_time = times - stop_time;
   document.getElementById("amountofres").innerHTML = resultTime.length;
   document.getElementById("timeofres").innerHTML = timeToString(times);
-  document.getElementById("timeofavarage").innerHTML = timeToString(
-    Number(times / resultTime.length)
-  );
+  document.getElementById("amountofrestimeer").innerHTML =
+    timeToString(pure_time);
+  document.getElementById("amountofrestime").innerHTML =
+    timeToString(stop_time);
+  document.getElementById("timeofavarage").innerHTML =
+    timeToString(avaragetime);
 }
 
 function compareNumbers(a, b) {
@@ -103,13 +110,16 @@ function sortResults(arr) {
   printResult(arr);
 }
 function copyElements() {
-  sortResults(resultarr);
-  let time = Number(endTime - startTime);
-  let avarage = Number(time / resultarr.length);
   let text = "";
   for (let i = 0; i < resultarr.length; i++) {
     const element = resultarr[i];
-    if (element.time > avarage) text = text + " " + memoNums[element.counter];
+    if (element.time > avaragetime)
+      text = text + " " + memoNums[element.counter];
   }
   navigator.clipboard.writeText(text.trim());
+}
+function copyTime() {
+  navigator.clipboard.writeText(
+    timeToString(pure_time) + "/" + timeToString(avaragetime)
+  );
 }
